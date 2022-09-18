ac06aaf568493aa5c80ac61a4beeedaf4acc1a04
David Mehren <git@herrmehren.de>
2022-09-18 18:20:32 +0200

    fix(public/notes-controller): extract canEdit parameter from body
    Signed-off-by: David Mehren <git@herrmehren.de>

--- src/api/public/notes/notes.controller.ts
+++ src/api/public/notes/notes.controller.ts
@@ -267,7 +267,7 @@ export class NotesController {
     @RequestUser() user: User,
     @RequestNote() note: Note,
     @Param('userName') username: string,
-    @Body() canEdit: boolean,
+    @Body('canEdit') canEdit: boolean,
   ): Promise<NotePermissionsDto> {
     const permissionUser = await this.userService.getUserByUsername(username);
     const returnedNote = await this.permissionService.setUserPermission(
@@ -320,7 +320,7 @@ export class NotesController {
   @OpenApi(
     {
       code: 200,
-      description: 'Set the permissions for a user on a note',
+      description: 'Set the permissions for a group on a note',
       dto: NotePermissionsDto,
     },
     403,
@@ -330,7 +330,7 @@ export class NotesController {
     @RequestUser() user: User,
     @RequestNote() note: Note,
     @Param('groupName') groupName: string,
-    @Body() canEdit: boolean,
+    @Body('canEdit') canEdit: boolean,
   ): Promise<NotePermissionsDto> {
     const permissionGroup = await this.groupService.getGroupByName(groupName);
     const returnedNote = await this.permissionService.setGroupPermission(
